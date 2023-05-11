a62d5db66cbbfb03d089461a81739d2ddea7bba7
xwedx00 <117844799+xwedx00@users.noreply.github.com>
2023-05-11 16:19:01 +0000

    Vercel Prod Fix

--- src/pages/scene-search.tsx
+++ src/pages/scene-search.tsx
@@ -10,7 +10,7 @@ import React, { useCallback, useState } from "react";
 import { CgArrowLongRight } from "react-icons/cg";
 import { MdOutlineRestartAlt } from "react-icons/md";
 import { ImageType } from "react-images-uploading";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
 import Section from "@/components/shared/Section";
 
 const TracePage = () => {
