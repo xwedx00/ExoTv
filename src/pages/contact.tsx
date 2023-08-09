b5140fc791d82673e811e5c9a6e814e72de59733
xwedx00 <xwedx00@protonmail.com>
2023-08-09 18:38:48 +0500

    Removed _error.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,5 @@
 //@ts-nocheck
 import Head from "@/components/shared/Head";
-import classNames from "classnames";
 import React from "react";
 
 export default function Contact() {
@@ -8,7 +7,7 @@ export default function Contact() {
     <div className="px-4 md:px-12 pt-20 space-y-4">
       <Head title="Contact - Exoexs" description="Contact Page." />
 
-      <h1 className="text-2xl font-bold">Contact Us</h1>
+      <h1 className="text-2xl font-bold">Contact Me</h1>
 
       <p className="text-lg">You can contact @ xwedx00@protonmail.com
       </p>
